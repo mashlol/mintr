193bc20cf0fb6d1945e2deddf2c91a7c01c65cd8
Kevin Bedi <kevin@kevinbedi.com>
2015-05-09 18:37:20 -0400

    Improve uptime to include hours, and use /sbin/init start time to determine uptime

--- monitors/uptime.js
+++ monitors/uptime.js
@@ -1,9 +1,16 @@
 var os = require('os');
+var exec = require('child_process').exec;
 
 var Uptime = {};
 
 Uptime.monitor = function(history, callback) {
-  callback(os.uptime());
+  exec('ps -eo lstart,args | grep /sbin/init', function(error, result) {
+    var processTextIndex = result.indexOf('/sbin/init');
+    var dateText = result.substring(0, processTextIndex);
+    var uptime = Date.now() - new Date(dateText).getTime();
+
+    callback(uptime);
+  });
 };
 
 Uptime.history = false;
--- public/js/widgets/uptime.js
+++ public/js/widgets/uptime.js
@@ -21,31 +21,32 @@
   };
 
   UptimeWidget.prototype.addData = function(data) {
-    var uptimeMilli = data.uptime * 1000;
+    var uptimeMilli = data.uptime;
     var duration = moment.duration(uptimeMilli);
 
     var years = duration.years();
     var months = duration.months();
     var days = duration.days();
+    var hours = duration.hours();
     var minutes = duration.minutes();
     var seconds = duration.seconds();
 
     var times = [
-      [years, 'years'],
-      [months, 'months'],
-      [days, 'days'],
-      [minutes, 'minutes'],
-      [seconds, 'seconds'],
+      [years, 'year'],
+      [months, 'month'],
+      [days, 'day'],
+      [hours, 'hour'],
+      [minutes, 'minute'],
+      [seconds, 'second'],
     ];
 
-    var timeHadValue = false;
     var display = "";
     for (var x = 0; x < times.length; x++) {
       var time = times[x];
-      if (timeHadValue || time[0]) {
-        display += time[0] + " " + time[1] +
+      if (time[0]) {
+        display += time[0] + ' ' + time[1] +
+          (time[0] !== 1 ? 's' : '') +
           (x !== times.length - 1 ? ', ' : '');
-        timeHadValue = true;
       }
     }
 
